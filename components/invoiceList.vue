bb861ba994e7e2377c4c277238c9882a8e7ecca5
julesprezrdlf <julioprezrdlf@gmail.com>
2022-07-15 19:09:49 +0200

    cleaning

--- components/invoiceList.vue
+++ components/invoiceList.vue
@@ -2,87 +2,61 @@
   <div class="containerInvoice mt-5">
     <div class="invoices text-xs font-semibold my-5">INVOICES</div>
     <hr />
-    <div v-for="item in jsonData" :key="item.id">
+    <div v-for="item in output" :key="item.id">
       <div class="billContainer mt-2 flex flex-row">
-             <p>{{ item.date }}</p>
-                 <div class="paid text-center text-xs font-semibold rounded-md align-middle" v-if="item.paid">
-                  <h1>PAID</h1>
-                 </div>
+        <p>{{ item.date }}</p>
+        <div
+          class="paid text-center text-xs font-semibold rounded-md align-middle"
+          v-if="item.paid"
+        >
+          <h1>PAID</h1>
+        </div>
 
-                  <div class="unpaid text-center text-xs font-semibold rounded-md align-middle" v-if="item.paid === false">
-                   <h1>UNPAID</h1>
-                  </div>
-        
-             </div>
-         <p class="mb-2 font-normal text-xs">Invoice #{{item.number}}</p>
+        <div
+          class="unpaid text-center text-xs font-semibold rounded-md align-middle"
+          v-if="item.paid === false"
+        >
+          <h1>UNPAID</h1>
+        </div>
+      </div>
+      <p class="mb-2 font-normal text-xs">Invoice #{{ item.number }}</p>
       <hr />
     </div>
-
-    {{ jsonData }}
-   <p>hola</p>
-    {{output}}
 
   </div>
 </template>
 
 <script>
-import invoices from "../assets/invoices.json";
 export default {
   data() {
     return {
-      jsonData: invoices,
-      output:{}
+      output: {},
     };
   },
   methods: {
-
     getJson: function () {
       fetch("https://julesprezrdlf.github.io/testSharp/invoices.json")
-    //   fetch("../static/invoices") MANTENER COMENTADO
         .then((response) => response.json())
         .then((data) => {
-            console.log("esto es data")
+          console.log("esto es data");
           console.log(data);
+        });
+    },
+
+    prueba() {
+      console.log("holaaaa");
+    },
+    fetchJson: function () {
+      fetch("https://julesprezrdlf.github.io/testSharp/invoices.json")
+        .then((res) => {
+          return res.json();
         })
-        // .then(this.setResults) MANTENER COMENTADO
-        ;
-        
-      
+        .then(this.setResults);
     },
-    // ,                           MANTENER COMENTADO
-    // setResults(results){        MANTENER COMENTADO
-    //     this.output=results;    MANTENER COMENTADO
-    // }                           MANTENER COMENTADO
-    prueba(){
-    console.log("holaaaa");
-    },
-fetchJson: function () {
-      
-          fetch("https://julesprezrdlf.github.io/testSharp/invoices.json")
-          .then(res=>{
-            return res.json();
-          }).then(this.setResults);
-      
-    },
-    setResults(results){
+    setResults(results) {
       this.output = results;
     },
-
   },
-
-    
-
-//   fetchJson: function () {
-      
-//           fetch("https://julesprezrdlf.github.io/testSharp/invoices.json")
-//           .then(res=>{
-//             return res.json();
-//           }).then(this.setResults);
-      
-//     },
-//     setResults(results){
-//       this.output = results;
-//     },
 
   beforeMount() {
     this.getJson();
@@ -96,7 +70,7 @@ fetchJson: function () {
 .unpaid {
   background-color: #f4541d;
   color: #fff;
-  
+
   padding: 2px, 6px, 2px, 6px;
   width: 59px;
   height: 20px;
